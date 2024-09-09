09170c1c95a9560782d76bdfb2d9241e50ff854f
comavius <comavius@proton.me>
2024-09-09 19:08:57 +0900

    🎨 get user and group from stat

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env 
+#!/usr/bin/env
 source .env.dev
 generate-api-crate () {
     rm -rf "$PWD"/"$1"
@@ -8,9 +8,9 @@ generate-api-crate () {
         -i "$2" \
         -g rust \
         -o /local/"$1"
-    sudo chown -R "$3":"$3" "$PWD"/"$1"
+    sudo chown -R "$(stat -c %U .)":"$(stat -c %G .)" "$PWD"/"$1"
 }
 
-generate-api-crate backend-api https://raw.githubusercontent.com/traP-jp/traO-Judge-docs/"$API_VERSION"/api/backend/to_judge.yaml "$USER"
-generate-api-crate judge-api https://raw.githubusercontent.com/traP-jp/traO-Judge-docs/"$API_VERSION"/api/judge/to_backend.yaml "$USER"
+generate-api-crate backend-api https://raw.githubusercontent.com/traP-jp/traO-Judge-docs/"$API_VERSION"/api/backend/to_judge.yaml
+generate-api-crate judge-api https://raw.githubusercontent.com/traP-jp/traO-Judge-docs/"$API_VERSION"/api/judge/to_backend.yaml
 
