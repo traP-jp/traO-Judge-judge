b064f2d0d96d4176c9e5074a2ffb0d9dd6beb81e
comavius <comavius@proton.me>
2024-09-09 16:56:00 +0900

    🎨 get api version from env.dev

--- generate.sh
+++ generate.sh
@@ -1,12 +1,5 @@
-#!/usr/bin/env bash
-
-if [ -z "$1" ]; then
-    echo "Usage: generate.sh <branch or tag>"
-    exit 1
-else
-    BRANCH="$1"
-fi
-
+#!/usr/bin/env 
+source .env.dev
 generate-api-crate () {
     rm -rf "$PWD"/"$1"
     mkdir -p "$PWD"/"$1"
@@ -18,6 +11,6 @@ generate-api-crate () {
     sudo chown -R "$3":"$3" "$PWD"/"$1"
 }
 
-generate-api-crate backend-api https://raw.githubusercontent.com/traP-jp/traO-Judge-docs/"$BRANCH"/api/backend/to_judge.yaml "$USER"
-generate-api-crate judge-api https://raw.githubusercontent.com/traP-jp/traO-Judge-docs/"$BRANCH"/api/judge/to_backend.yaml "$USER"
+generate-api-crate backend-api https://raw.githubusercontent.com/traP-jp/traO-Judge-docs/"$API_VERSION"/api/backend/to_judge.yaml "$USER"
+generate-api-crate judge-api https://raw.githubusercontent.com/traP-jp/traO-Judge-docs/"$API_VERSION"/api/judge/to_backend.yaml "$USER"
 
