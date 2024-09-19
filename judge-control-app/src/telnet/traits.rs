dfaec5a36ff456a64438a94b9f7ccae9a5d9a827
comavius <comavius@proton.me>
2024-09-19 18:52:37 +0900

    🚧 exec returns future

--- judge-control-app/src/telnet/traits.rs
+++ judge-control-app/src/telnet/traits.rs
@@ -1,7 +1,8 @@
 use anyhow::Result;
+use std::future::Future;
 use std::net::ToSocketAddrs;
 
 pub trait Telnet {
     fn new<Addr: ToSocketAddrs>(addr: Addr) -> Self;
-    fn exec(&mut self, cmd: &str) -> Result<String>;
+    fn exec(&mut self, cmd: &str) -> Result<impl Future<Output = Result<String>>>;
 }
