9b9f6bb7640030e7234d57317ee5b7c482176718
hayatroid <hayatroid@gmail.com>
2024-09-29 22:41:03 +0900

    `InitArgs` の実装

--- judge-control-app/src/custom_file/traits.rs
+++ judge-control-app/src/custom_file/traits.rs
@@ -1,6 +1,9 @@
 #![allow(drop_bounds)]
 use anyhow::{anyhow, Result};
-use std::path::PathBuf;
+use std::{
+    io::{Read, Write},
+    path::PathBuf,
+};
 use uuid::Uuid;
 
 pub trait File: Sized + Drop {
@@ -34,25 +37,29 @@ pub struct TextFile {
 }
 
 impl File for Directory {
-    type InitArgs = todo!();
-    fn new(path: PathBuf, args: Self::InitArgs) -> Result<Self> {
+    type InitArgs = ();
+    fn new(path: PathBuf, _args: Self::InitArgs) -> Result<Self> {
         std::fs::create_dir_all(&path)?;
         Ok(Directory { path })
     }
-    fn get_hardlink_to(&self, path: PathBuf) -> Result<Self> {
+    fn get_hardlink_to(&self, _path: PathBuf) -> Result<Self> {
         Err(anyhow!("hard link not allowed for directory"))
     }
 }
 
 impl File for TextFile {
-    type InitArgs = todo!();
+    type InitArgs = Option<String>;
     fn new(path: PathBuf, args: Self::InitArgs) -> Result<Self> {
         std::fs::File::create(&path)?;
-        Ok(TextFile { path })
+        let res = TextFile { path };
+        if let Some(contents) = args {
+            res.write(contents)?;
+        }
+        Ok(res)
     }
     fn get_hardlink_to(&self, path: PathBuf) -> Result<Self> {
         std::fs::hard_link(&self.path, &path)?;
-        TextFile::new(path, todo!())
+        TextFile::new(path, None)
     }
 }
 
@@ -70,14 +77,14 @@ impl Drop for TextFile {
 
 impl TextFile {
     fn read(&self) -> Result<String> {
-        let mut f = OpenOptions::new().read(true).open(&self.path)?;
+        let mut file = std::fs::OpenOptions::new().read(true).open(&self.path)?;
         let mut contents = String::new();
-        f.read_to_string(&mut contents)?;
+        file.read_to_string(&mut contents)?;
         Ok(contents)
     }
     fn write(&self, contents: String) -> Result<()> {
-        let mut f = OpenOptions::new().write(true).open(&self.path)?;
-        f.write_all(contents.as_bytes())?;
+        let mut file = std::fs::OpenOptions::new().write(true).open(&self.path)?;
+        file.write_all(contents.as_bytes())?;
         Ok(())
     }
 }
