fa7dc0259d1d2bed82291b1d6a5a3d6a64d8b793
hayatroid <hayatroid@gmail.com>
2024-09-30 19:32:54 +0900

    `get` を `create` に

--- judge-control-app/src/custom_file/traits.rs
+++ judge-control-app/src/custom_file/traits.rs
@@ -6,11 +6,11 @@ use uuid::Uuid;
 pub trait File: Sized + Drop {
     type InitArgs;
     fn new(path: PathBuf, args: Self::InitArgs) -> Result<Self>;
-    fn get_hardlink_to(&self, path: PathBuf) -> Result<Self>;
+    fn create_hardlink_to(&self, path: PathBuf) -> Result<Self>;
 }
 
 pub trait FileFactory: Sized {
     fn new(path: PathBuf) -> Result<Self>;
-    fn get_file<FileType: File>(&self, uuid: Uuid, args: FileType::InitArgs) -> Result<FileType>;
-    fn get_hardlink_of<FileType: File>(&self, uuid: Uuid, original: FileType) -> Result<FileType>;
+    fn create_file<FileType: File>(&self, uuid: Uuid, args: FileType::InitArgs) -> Result<FileType>;
+    fn create_hardlink_of<FileType: File>(&self, uuid: Uuid, original: FileType) -> Result<FileType>;
 }
