eb73e564f98903bf55f98892095cca3c4d1d4d65
Noya2 <107465724+noya2ruler@users.noreply.github.com>
2024-10-22 17:56:04 +0900

    format all

--- judge-control-app/judge-api-server/src/server/mod.rs
+++ judge-control-app/judge-api-server/src/server/mod.rs
@@ -12,7 +12,6 @@ use crate::{header, types::*};
 #[allow(unused_imports)]
 use crate::{apis, models};
 
-
 /// Setup API Server.
 pub fn new<I, A>(api_impl: I) -> Router
 where
@@ -21,88 +20,71 @@ where
 {
     // build our application with a route
     Router::new()
-        .route("/v1/judge",
-            post(judge::<I, A>)
-        )
+        .route("/v1/judge", post(judge::<I, A>))
         .with_state(api_impl)
 }
 
-    #[derive(validator::Validate)]
-    #[allow(dead_code)]
-    struct JudgeBodyValidator<'a> {
-            #[validate(nested)]
-          body: &'a models::Judge,
-    }
-
+#[derive(validator::Validate)]
+#[allow(dead_code)]
+struct JudgeBodyValidator<'a> {
+    #[validate(nested)]
+    body: &'a models::Judge,
+}
 
 #[tracing::instrument(skip_all)]
 fn judge_validation(
-        body: models::Judge,
-) -> std::result::Result<(
-        models::Judge,
-), ValidationErrors>
-{
-              let b = JudgeBodyValidator { body: &body };
-              b.validate()?;
+    body: models::Judge,
+) -> std::result::Result<(models::Judge,), ValidationErrors> {
+    let b = JudgeBodyValidator { body: &body };
+    b.validate()?;
 
-Ok((
-    body,
-))
+    Ok((body,))
 }
 /// Judge - POST /v1/judge
 #[tracing::instrument(skip_all)]
 async fn judge<I, A>(
-  method: Method,
-  host: Host,
-  cookies: CookieJar,
- State(api_impl): State<I>,
-          Json(body): Json<models::Judge>,
+    method: Method,
+    host: Host,
+    cookies: CookieJar,
+    State(api_impl): State<I>,
+    Json(body): Json<models::Judge>,
 ) -> Result<Response, StatusCode>
 where
     I: AsRef<A> + Send + Sync,
     A: apis::judge::Judge,
 {
+    #[allow(clippy::redundant_closure)]
+    let validation = tokio::task::spawn_blocking(move || judge_validation(body))
+        .await
+        .unwrap();
 
-      #[allow(clippy::redundant_closure)]
-      let validation = tokio::task::spawn_blocking(move ||
-    judge_validation(
-          body,
-    )
-  ).await.unwrap();
-
-  let Ok((
-      body,
-  )) = validation else {
-    return Response::builder()
+    let Ok((body,)) = validation else {
+        return Response::builder()
             .status(StatusCode::BAD_REQUEST)
             .body(Body::from(validation.unwrap_err().to_string()))
             .map_err(|_| StatusCode::BAD_REQUEST);
-  };
+    };
 
-  let result = api_impl.as_ref().judge(
-      method,
-      host,
-      cookies,
-              body,
-  ).await;
+    let result = api_impl.as_ref().judge(method, host, cookies, body).await;
 
-  let mut response = Response::builder();
+    let mut response = Response::builder();
 
-  let resp = match result {
-                                            Ok(rsp) => match rsp {
-                                                apis::judge::JudgeResponse::Status200_OK
-                                                => {
-                                                  let mut response = response.status(200);
-                                                  response.body(Body::empty())
-                                                },
-                                            },
-                                            Err(_) => {
-                                                // Application code returned an error. This should not happen, as the implementation should
-                                                // return a valid response.
-                                                response.status(500).body(Body::empty())
-                                            },
-                                        };
+    let resp = match result {
+        Ok(rsp) => match rsp {
+            apis::judge::JudgeResponse::Status200_OK => {
+                let mut response = response.status(200);
+                response.body(Body::empty())
+            }
+        },
+        Err(_) => {
+            // Application code returned an error. This should not happen, as the implementation should
+            // return a valid response.
+            response.status(500).body(Body::empty())
+        }
+    };
 
-                                        resp.map_err(|e| { error!(error = ?e); StatusCode::INTERNAL_SERVER_ERROR })
+    resp.map_err(|e| {
+        error!(error = ?e);
+        StatusCode::INTERNAL_SERVER_ERROR
+    })
 }
-
