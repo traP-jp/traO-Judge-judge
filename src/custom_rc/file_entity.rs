c45f7a3a4b6e0042861e715b36336e1d9117e11b
comavius <comavius@proton.me>
2024-12-02 01:21:50 +0900

    ✨ file_entity

---
+++ src/custom_rc/file_entity.rs
@@ -0,0 +1,69 @@
+use anyhow::{Context, Result};
+use std::path::PathBuf;
+pub struct TextFileEntity {
+    pub path: PathBuf,
+}
+
+impl TextFileEntity {
+    pub fn new(path: PathBuf, content: &str) -> Result<Self> {
+        std::fs::write(&path, content)
+            .with_context(|| {
+                format!(
+                    "Failed to write to file while creating TextFileEntity : {:?}",
+                    path
+                )
+            })
+            .map(|_| Self { path })
+    }
+}
+
+impl Drop for TextFileEntity {
+    fn drop(&mut self) {
+        let result = std::fs::remove_file(&self.path).context(format!(
+            "Failed to remove file while dropping TextFileEntity : {:?}",
+            self.path
+        ));
+        match result {
+            Ok(_) => {
+                return;
+            }
+            Err(e) => {
+                eprintln!("{:?}", e);
+            }
+        }
+    }
+}
+
+pub struct DirectoryEntity {
+    pub path: PathBuf,
+}
+
+impl DirectoryEntity {
+    pub fn new(path: PathBuf) -> Result<Self> {
+        std::fs::create_dir_all(&path)
+            .with_context(|| {
+                format!(
+                    "Failed to create directory while creating DirectoryEntity : {:?}",
+                    path
+                )
+            })
+            .map(|_| Self { path })
+    }
+}
+
+impl Drop for DirectoryEntity {
+    fn drop(&mut self) {
+        let result = std::fs::remove_dir_all(&self.path).context(format!(
+            "Failed to remove directory while dropping DirectoryEntity : {:?}",
+            self.path
+        ));
+        match result {
+            Ok(_) => {
+                return;
+            }
+            Err(e) => {
+                eprintln!("{:?}", e);
+            }
+        }
+    }
+}
