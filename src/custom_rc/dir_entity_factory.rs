55650b91ec7cba30eb608c5eb75cb1794ccd217f
comavius <comavius@proton.me>
2024-12-02 01:22:35 +0900

    ✨ dir entity factory

---
+++ src/custom_rc/dir_entity_factory.rs
@@ -0,0 +1,18 @@
+use std::path::PathBuf;
+use anyhow::Result;
+use crate::custom_rc::file_entity::DirectoryEntity;
+use std::sync::Arc;
+
+pub struct DirEntityFactory {
+    pub path: PathBuf,
+}
+
+impl DirEntityFactory {
+    pub fn new(path: PathBuf) -> Self {
+        Self { path }
+    }
+
+    pub fn get_dir_entity(&self) -> Result<DirectoryEntity> {
+        Ok(DirectoryEntity::new(self.path.clone())?)
+    }
+}
