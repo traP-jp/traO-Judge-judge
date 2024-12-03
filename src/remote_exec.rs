758674a8ea9d2fe063ae0fb9d62ac6c76ab48f8d
comavius <comavius@proton.me>
2024-12-03 18:34:39 +0900

    ♻️ 🎨 refactor remote-exec

--- src/remote_exec.rs
+++ src/remote_exec.rs
@@ -1,2 +1,22 @@
-pub mod ssh;
-pub mod traits;
+use anyhow::Result;
+
+pub struct SuccessfulExecutionOutput {
+    stdout: String,
+    stderr: String,
+    exit_code: i32,
+}
+
+pub enum ExecutionOutput {
+    Success(SuccessfulExecutionOutput),
+    FailureByWriter(anyhow::Error),
+}
+
+pub trait RemoteExecutor {
+    fn execute(
+        &self,
+        cmd: &str,
+        envs: std::collections::HashMap<String, String>,
+        connection_time_limit: std::time::Duration,
+        execution_time_limit: std::time::Duration
+    ) -> Result<ExecutionOutput>;
+}
