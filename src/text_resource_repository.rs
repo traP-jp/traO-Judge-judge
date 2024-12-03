a60b1da29147e16bb8ac6d6f71044926d51195a9
comavius <comavius@proton.me>
2024-12-03 18:34:12 +0900

    🎨 change module structure of text_rc_repo

--- src/text_resource_repository.rs
+++ src/text_resource_repository.rs
@@ -1,1 +1,5 @@
-pub mod traits;
+use anyhow::Result;
+
+pub trait TextResourceRepository<KeyType> {
+    fn get_text(&self, key: &KeyType) -> Result<String>;
+}
--- src/text_resource_repository/traits.rs
+++
@@ -1,5 +0,0 @@
-use anyhow::Result;
-
-pub trait TextResourceRepository<KeyType> {
-    fn get_text(&self, key: &KeyType) -> Result<String>;
-}
