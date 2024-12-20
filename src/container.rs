f006036908b0d2dbd3dcb29acd855cc051a9e7ae
comavius <comavius@proton.me>
2024-12-20 15:50:00 +0900

    ♻️ refactor container

--- src/container.rs
+++ src/container.rs
@@ -1,20 +1,22 @@
-#![allow(clippy::module_inception)]
-use crate::custom_rc::FileLink;
-use crate::custom_rc::SymlinkLink;
 use crate::remote_exec::ExecutionOutput;
 use anyhow::Result;
 use std::collections::HashMap;
-pub use std::path::PathBuf;
-use tokio::sync::MutexGuard;
+use crate::custom_rc::{ReadonlyFile, WriteableFile, FileLink};
 
 pub trait Container {
-    async fn execute<'a, FileLinkType: FileLink, SymlinkLinkType: SymlinkLink<'a, FileLinkType>>(
+    async fn execute<
+        ReadonlyFileType: ReadonlyFile,
+        WriteableFileType: WriteableFile<ReadonlyFileType>,
+        ReadonlyFileLinkType: FileLink<ReadonlyFileType>,
+        WriteableFileLinkType: FileLink<WriteableFileType>,
+    > (
         &self,
         cmd: &str,
         envs: HashMap<String, String>,
         connection_time_limit: std::time::Duration,
         execution_time_limit: std::time::Duration,
-        file_links: HashMap<PathBuf, MutexGuard<'a, FileLinkType>>,
+        readonly_files: &Vec<ReadonlyFileType>,
+        writeable_files: &Vec<WriteableFileType>,
     ) -> Result<ExecutionOutput>;
 }
 
