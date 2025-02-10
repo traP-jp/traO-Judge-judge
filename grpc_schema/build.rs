d5282761230134c32a690872b567a8f99b5a4213
comavius <comavius@proton.me>
2025-02-10 16:55:54 +0900

    ✨ grpc gen scripts

---
+++ grpc_schema/build.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let schema_dir = std::path::Path::new("../traO-Judge-docs/api").canonicalize().unwrap();
+    let out_dir = std::path::Path::new("./src").canonicalize().unwrap();
+    let schema_path = std::fs::read_dir(&schema_dir)
+        .unwrap()
+        .map(|entry| entry.unwrap().path())
+        .find(|path| path.extension() == Some(std::ffi::OsStr::new("proto")))
+        .unwrap();
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(false)
+        .out_dir(out_dir)
+        .compile_protos(&[schema_path], &[schema_dir])
+        .unwrap();
+}
