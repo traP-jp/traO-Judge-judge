380af223a8ab4479fb7040ca8507a0d83d012bb7
comavius <comavius@proton.me>
2025-02-10 17:10:55 +0900

    💚 add envvar CODEGEN_SKIP for production or ci environments

--- grpc_schema/build.rs
+++ grpc_schema/build.rs
@@ -1,4 +1,7 @@
 fn main() {
+    if std::env::var("CODEGEN_SKIP").is_ok() {
+        return;
+    }
     let schema_dir = std::path::Path::new("../traO-Judge-docs/api").canonicalize().unwrap();
     println!("cargo:rerun-if-changed={}", schema_dir.to_str().unwrap());
     let out_dir = std::path::Path::new("./src").canonicalize().unwrap();
