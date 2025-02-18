85464153511361c40602cbb3c79cbfead32beaf4
comavius <comavius@proton.me>
2025-02-19 00:41:54 +0900

    ✨ judge output parsing logic

---
+++ lib/judge_core/src/judge_output.rs
@@ -0,0 +1,61 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum JudgeStatus {
+    AC,
+    WA,
+    TLE,
+    MLE,
+    OLE,
+    RE,
+    CE,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum ContinueStatus {
+    Continue,
+    Stop,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct DisplayableJudgeResult {
+    pub status: JudgeStatus,
+    pub time: f64,
+    pub memory: f64,
+    pub score: i64,
+    pub message: Option<String>,
+    pub continue_status: ContinueStatus,
+}
+
+/// This returns from exec container as stdout
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum JudgeReport {
+    Displayable(DisplayableJudgeResult),
+    Hidden,
+}
+
+/// This is the final response from judge
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum JudgeResponse {
+    Report(JudgeReport),
+    EarlyExit,
+    Error(String),
+}
+
+#[derive(Debug, Clone, thiserror::Error)]
+pub enum JudgeOutputParseError {
+    #[error("Invalid JSON: {0}")]
+    InvalidJson(String),
+    #[error("Non-zero exit code")]
+    InvalidExitCode,
+}
+
+pub fn parse(output: &std::process::Output) -> Result<JudgeReport, JudgeOutputParseError> {
+    let stdout = String::from_utf8_lossy(&output.stdout).to_string();
+    if !output.status.success() {
+        return Err(JudgeOutputParseError::InvalidExitCode);
+    }
+    let judge_report: JudgeReport = serde_json::from_str(&stdout)
+        .map_err(|e| JudgeOutputParseError::InvalidJson(e.to_string()))?;
+    Ok(judge_report)
+}
