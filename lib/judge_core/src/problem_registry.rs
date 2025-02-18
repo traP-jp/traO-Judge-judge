325f6650cca1f812608334d0d3cb9d6b22fd28c1
comavius <comavius@proton.me>
2025-02-18 16:19:57 +0900

    ✨ add problem registry

---
+++ lib/judge_core/src/problem_registry.rs
@@ -0,0 +1,13 @@
+use crate::{identifiers::ResourceId, procedure::*};
+use futures::Future;
+use anyhow::Result;
+
+/// ProblemRegistryServer uploads contents of problems to the registry in webservice-backend server.
+pub trait ProblemRegistryServer {
+    fn register(&self, problem: writer_schema::Procedure) -> impl Future<Output = Result<registered::Procedure>>;
+}
+
+/// ProblemRegistryClient fetches contents of problems from the registry in judge server.
+pub trait ProblemRegistryClient {
+    fn fetch(&self, resource_id: ResourceId) -> impl Future<Output = Result<String>>;
+}
