2171e3decd72e8cad7427338c4eb690439b770db
hayatroid <hayatroid@gmail.com>
2025-03-10 06:38:02 +0900

    😼 add some needed functions

--- lib/jobapi/src/aws.rs
+++ lib/jobapi/src/aws.rs
@@ -1,12 +1,26 @@
-use std::{net::Ipv4Addr, path::PathBuf};
+use std::net::Ipv4Addr;
 
+use judge_core::job;
 use uuid::Uuid;
 
-trait AwsClient: Sized {
-    async fn new() -> Result<Self, anyhow::Error>;
+trait AwsClient {
     async fn create_instance(&mut self, instance_id: Uuid) -> Result<Ipv4Addr, anyhow::Error>;
     async fn terminate_instance(&mut self, instance_id: Uuid) -> Result<(), anyhow::Error>;
-    async fn touch_file(&self, instance_id: Uuid, file_path: PathBuf) -> Result<(), anyhow::Error>;
-    async fn remove_file(&self, instance_id: Uuid, file_path: PathBuf)
-        -> Result<(), anyhow::Error>;
+    async fn place_file(
+        &self,
+        outcome_id: Uuid,
+        file_conf: job::FileConf,
+    ) -> Result<(), anyhow::Error>;
+    async fn push_outcome_to_instance_directory(
+        &self,
+        instance_id: Uuid,
+        outcome_id: Uuid,
+    ) -> Result<(), anyhow::Error>;
+    async fn pull_outcome_from_instance_directory(
+        &self,
+        instance_id: Uuid,
+        outcome_id: Uuid,
+    ) -> Result<(), anyhow::Error>;
+    async fn clear_instance_directory(&self, instance_id: Uuid) -> Result<(), anyhow::Error>;
+    async fn remove_outcome_directory(&self, outcome_id: Uuid) -> Result<(), anyhow::Error>;
 }
