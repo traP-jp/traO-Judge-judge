85877f31e1ed3eafb6b4422d6222b4722cbf4961
comavius <comavius@proton.me>
2025-03-12 03:32:38 +0900

    ✨ aarch cross platform build

--- flake.nix
+++ flake.nix
@@ -14,6 +14,12 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        aarch64-pkgs = import nixpkgs {
+          inherit system;
+          crossSystem = {
+            system = "aarch64-linux";
+          };
+        };
       in
       {
         devShell = pkgs.mkShell {
@@ -22,6 +28,26 @@
           ];
         };
         formatter = pkgs.nixfmt-rfc-style;
+        packages.aarch64-crossPlatform =
+          with aarch64-pkgs;
+          rustPlatform.buildRustPackage {
+            name = "traO-Judge-judge";
+            src = ./.;
+            hostPlatform = "aarch64-linux";
+            buildPlatform = system;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              outputHashes = {
+                "async-sqlx-session-0.4.0" = "sha256-/iNCvfyqJP72z7TXj0p+epGCLYNClVAxWTcpGJ5mrmc=";
+              };
+            };
+            env = {
+              OPENSSL_DIR = "${openssl_3.dev}";
+              OPENSSL_LIB_DIR = "${openssl_3.out}/lib";
+              OPENSSL_INCLUDE_DIR = "${openssl_3.dev}/include";
+              CODEGEN_SKIP = "true";
+            };
+          };
       }
     );
 }
