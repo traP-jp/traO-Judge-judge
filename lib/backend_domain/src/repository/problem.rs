d654ebe59d270266438dbfc809ddb02e397c3e01
PonponJuice <ponjuice2021@gmail.com>
2025-03-11 16:12:15 +0900

    ✨ add auth usecases tests

--- lib/backend_domain/src/repository/problem.rs
+++ lib/backend_domain/src/repository/problem.rs
@@ -2,6 +2,8 @@ use axum::async_trait;
 
 use crate::model::problem::NormalProblem;
 
+
+#[cfg_attr(feature = "mockall", mockall::automock)]
 #[async_trait]
 pub trait ProblemRepository {
     async fn get_problem(&self, id: i64) -> anyhow::Result<Option<NormalProblem>>;
