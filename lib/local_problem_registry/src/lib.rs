26bef62cb52d1aa7b624368996bffcd349088431
comavius <comavius@proton.me>
2025-03-14 15:34:31 +0900

    🚚 rename one-proc only registry

--- lib/local_problem_registry/src/lib.rs
+++ lib/local_problem_registry/src/lib.rs
@@ -1,17 +1,2 @@
-pub mod registry_client;
-pub mod registry_server;
-
-pub fn new_registry() -> (
-    registry_server::RegistryServer,
-    registry_client::RegistryClient,
-) {
-    let registry = std::sync::Arc::new(tokio::sync::Mutex::new(std::collections::HashMap::new()));
-    let server = registry_server::RegistryServer {
-        registry: registry.clone(),
-        dep_id_to_name: std::sync::Arc::new(tokio::sync::Mutex::new(
-            std::collections::HashMap::new(),
-        )),
-    };
-    let client = registry_client::RegistryClient { registry };
-    (server, client)
-}
+pub mod one_proc;
+pub mod multi_proc;
---
+++ lib/local_problem_registry/src/one_proc/registry_server.rs
@@ -0,0 +1,49 @@
+use judge_core::{
+    logic::writer_schema_transpiler::transpile,
+    model::{problem_registry::*, procedure::*, *},
+};
+use std::collections::HashMap;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+#[derive(Debug, Clone)]
+pub struct RegistryServer {
+    pub(crate) registry: Arc<Mutex<HashMap<identifiers::ResourceId, String>>>,
+    pub(crate) dep_id_to_name: Arc<Mutex<HashMap<identifiers::DepId, String>>>,
+}
+
+#[axum::async_trait]
+impl ProblemRegistryServer for RegistryServer {
+    async fn register(
+        &self,
+        procedure: writer_schema::Procedure,
+    ) -> Result<registered::Procedure, RegistrationError> {
+        let (transpiled_procedure, content_to_register, dep_id_to_name) = transpile(procedure)?;
+        {
+            let mut registry = self.registry.lock().await;
+            for (resource_id, content) in content_to_register {
+                registry.insert(resource_id, content);
+            }
+        }
+        {
+            let mut dep_id_to_name_global = self.dep_id_to_name.lock().await;
+            for (dep_id, name) in dep_id_to_name {
+                dep_id_to_name_global.insert(dep_id, name);
+            }
+        }
+        Ok(transpiled_procedure)
+    }
+
+    async fn restore_name(&self, dep_id: identifiers::DepId) -> Option<String> {
+        let dep_id_to_name = self.dep_id_to_name.lock().await;
+        dep_id_to_name.get(&dep_id).cloned()
+    }
+
+    async fn remove(&self, procedure: registered::Procedure) -> Result<(), RemovalError> {
+        let mut registry = self.registry.lock().await;
+        for text in procedure.texts {
+            registry.remove(&text.resource_id);
+        }
+        Ok(())
+    }
+}
