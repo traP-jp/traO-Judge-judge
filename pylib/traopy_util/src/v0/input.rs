03ddd66a645d72ec973a92f98af31eb56ed46d57
comavius <comavius@proton.me>
2025-03-14 23:33:19 +0900

    📝 improve error logs

--- pylib/traopy_util/src/v0/input.rs
+++ pylib/traopy_util/src/v0/input.rs
@@ -21,20 +21,22 @@ impl ExecutionMetadata {
     #[new]
     /// Read the input from environment variables.
     fn new() -> Self {
-        let time_limit_ms = std::env::var(env_var_exec::TIME_LIMIT_MS)
-            .ok()
-            .map(|s| s.parse::<f64>().expect("TIME_LIMIT_MS not a number"));
-        let memory_limit_kib = std::env::var(env_var_exec::MEMORY_LIMIT_KIB)
-            .ok()
-            .map(|s| s.parse::<i64>().expect("MEMORY_LIMIT_KIB not a number"));
+        let time_limit_ms = std::env::var(env_var_exec::TIME_LIMIT_MS).ok().map(|s| {
+            s.parse::<f64>()
+                .expect(format!("${} not a number", env_var_exec::TIME_LIMIT_MS).as_str())
+        });
+        let memory_limit_kib = std::env::var(env_var_exec::MEMORY_LIMIT_KIB).ok().map(|s| {
+            s.parse::<i64>()
+                .expect(format!("${} not a number", env_var_exec::MEMORY_LIMIT_KIB).as_str())
+        });
         let language = std::env::var(env_var_exec::LANGUAGE)
             .ok()
             .map(|s| s.to_string());
         let submission_path = std::env::var(env_var_exec::SOURCE_PATH)
-            .expect("SOURCE_PATH not set")
+            .expect(format!("${} not set", env_var_exec::SOURCE_PATH).as_str())
             .into();
         let output_path = std::env::var(env_var_exec::OUTPUT_PATH)
-            .expect("OUTPUT_PATH not set")
+            .expect(format!("${} not set", env_var_exec::OUTPUT_PATH).as_str())
             .into();
         Self {
             time_limit_ms,
