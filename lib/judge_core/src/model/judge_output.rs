12e84594cc1435f20d8ee548e83eb667d7a3ba36
comavius <comavius@proton.me>
2025-03-23 18:37:49 +0900

    🐛 chore: JudgeStatus Eq trait

--- lib/judge_core/src/model/judge_output.rs
+++ lib/judge_core/src/model/judge_output.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(PartialEq, Eq, Debug, Clone, Serialize, Deserialize)]
 pub enum JudgeStatus {
     AC,
     WA,
