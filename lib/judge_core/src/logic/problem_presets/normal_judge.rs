06d1f503769dc679023db361530b7eefbdbefc83
PonponJuice <ponjuice2021@gmail.com>
2025-03-23 22:40:44 +0900

    feat: add time and memory limit parameters to create_normal_judge_procedure

--- lib/judge_core/src/logic/problem_presets/normal_judge.rs
+++ lib/judge_core/src/logic/problem_presets/normal_judge.rs
@@ -7,6 +7,8 @@ pub struct NormalJudgeTestcase {
 }
 
 pub fn create_normal_judge_procedure(
+    time_limit_ms: i64,
+    memory_limit_kib: i64,
     testcases: Vec<NormalJudgeTestcase>,
 ) -> anyhow::Result<procedure::writer_schema::Procedure> {
     todo!()
