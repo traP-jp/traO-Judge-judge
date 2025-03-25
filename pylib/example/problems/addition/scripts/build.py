15a9d7b17c24ecf8236a6bf4ada15bf474e77d5a
comavius <comavius@proton.me>
2025-03-25 16:53:16 +0900

    ✨ updated problem example

--- pylib/example/problems/addition/scripts/build.py
+++ pylib/example/problems/addition/scripts/build.py
@@ -1,44 +1,51 @@
-#!python-traopy-v0
+#!/usr/bin/env python3
 import os
+import asyncio
 from traopy_util.util import v0 as trau # type: ignore[reportMissingModuleSource]
 from traopy_util.util import common as trau_common # type: ignore[reportMissingModuleSource]
 
-language_tag = trau_common.read_file_with_envvar("LANGUAGE_TAG")
-language_info = trau.get_language_info(language_tag)
-outcome_path = os.environ.get(trau.exec_job_outcome_path_envvar())
-source_path = os.environ.get("BUILD_SOURCE_PATH")
-
-exec_stats = trau.exec_with_stats(
-    cmd=language_info.compile,
-    envs={
-        trau.build_output_envvar(): outcome_path,
-        trau.build_source_envvar(): source_path,
-    },
-    time_limit_ms=30000,
-)
-if exec_stats is None:
-    json = trau.jsonify_displayable_output(
-        status=trau.JudgeStatus.CE,
-        time_ms=0,
-        memory_kib=0,
-        score=0,
-        continue_next=False,
+async def main():
+    language_tag = trau_common.read_file_with_envvar("LANGUAGE_TAG")
+    language_info = trau.get_language_info(language_tag)
+    outcome_path = os.environ.get(trau.exec_job_outcome_path_envvar())
+    source_path = os.environ.get("BUILD_SOURCE_PATH")
+    tempdir_path = os.environ.get("BUILD_TEMPDIR")
+    exec_stats = await trau.exec_with_stats(
+        cmd=language_info.compile,
+        envs={
+            trau.build_output_envvar(): f"{outcome_path}/main.out",
+            trau.build_source_envvar(): source_path,
+            trau.build_tempdir_envvar(): tempdir_path,
+        },
+        time_limit_ms=30000,
     )
-else:
-    if exec_stats.exit_code != 0:
+    if exec_stats is None:
         json = trau.jsonify_displayable_output(
             status=trau.JudgeStatus.CE,
-            time_ms=exec_stats.time_ms,
-            memory_kib=exec_stats.memory_kib,
+            time_ms=0,
+            memory_kib=0,
             score=0,
             continue_next=False,
         )
-    json = trau.jsonify_displayable_output(
-        status=trau.JudgeStatus.AC,
-        time_ms=exec_stats.time_ms,
-        memory_kib=exec_stats.memory_kib,
-        score=0,
-        continue_next=True,
-    )
+    else:
+        if exec_stats.exit_code != 0:
+            json = trau.jsonify_displayable_output(
+                status=trau.JudgeStatus.CE,
+                time_ms=exec_stats.time_ms,
+                memory_kib=exec_stats.memory_kib,
+                score=0,
+                continue_next=False,
+            )
+        else:
+            json = trau.jsonify_displayable_output(
+                status=trau.JudgeStatus.AC,
+                time_ms=exec_stats.time_ms,
+                memory_kib=exec_stats.memory_kib,
+                score=0,
+                continue_next=True,
+            )
 
-print(json)
+    print(json)
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- pylib/example/problems/addition/scripts/run.py
+++ pylib/example/problems/addition/scripts/run.py
@@ -1,39 +1,32 @@
-#!python-traopy-v0
+#!/usr/bin/env python3
 from traopy_util.util import v0 as trau # type: ignore[reportMissingModuleSource]
 from traopy_util.util import common as trau_common # type: ignore[reportMissingModuleSource]
+import asyncio
 import os
 
-language_tag = trau_common.read_file_with_envvar("LANGUAGE_TAG")
-time_limit_ms = int(trau_common.read_file_with_envvar("TIME_LIMIT_MS"))
-memory_limit_kib = int(trau_common.read_file_with_envvar("MEMORY_LIMIT_KIB"))
-input_file = trau_common.read_file_with_envvar("INPUT_FILE")
-expected_file = trau_common.read_file_with_envvar("EXPECTED_FILE")
-output_file = f"{trau_common.read_file_with_envvar('TEMP_DIR')}/output.txt"
-source_path = os.environ.get("BUILD_SOURCE_PATH")
-build_output_path = os.environ.get("BUILD_OUTPUT_PATH")
+async def main():
+    language_tag = trau_common.read_file_with_envvar("LANGUAGE_TAG")
+    time_limit_ms = int(trau_common.read_file_with_envvar("TIME_LIMIT_MS"))
+    memory_limit_kib = int(trau_common.read_file_with_envvar("MEMORY_LIMIT_KIB"))
+    input_file_path = os.environ.get("INPUT_FILE")
+    expected_file = trau_common.read_file_with_envvar("EXPECTED_FILE")
+    output_file_path = f"{os.environ.get('TEMP_DIR')}/output.txt"
+    source_path = os.environ.get("BUILD_SOURCE_PATH")
+    build_output_path = os.environ.get("BUILD_OUTPUT_PATH")
 
-language_info = trau.get_language_info(language_tag)
-command = f"{language_info.run} < {input_file} > {output_file}"
+    language_info = trau.get_language_info(language_tag)
+    command = f"{language_info.run} < {input_file_path} > {output_file_path}"
 
-exec_stats = trau.exec_with_stats(
-    cmd=command,
-    envs={
-        trau.build_output_envvar(): build_output_path,
-        trau.build_source_envvar(): source_path,
-    },
-    time_limit_ms=2000,
-)
-
-
-if exec_stats is None:
-    json = trau.jsonify_displayable_output(
-        status=trau.JudgeStatus.TLE,
-        time_ms=2000,
-        memory_kib=0,
-        score=0,
-        continue_next=True,
+    exec_stats = await trau.exec_with_stats(
+        cmd=command,
+        envs={
+            trau.build_output_envvar(): f"{build_output_path}/main.out",
+            trau.build_source_envvar(): source_path,
+        },
+        time_limit_ms=2000,
     )
-else:
+
+
     if exec_stats is None:
         json = trau.jsonify_displayable_output(
             status=trau.JudgeStatus.TLE,
@@ -43,17 +36,17 @@ else:
             continue_next=True,
         )
     else:
-        if exec_stats.exit_code != 0:
+        if exec_stats.time_ms > time_limit_ms:
             json = trau.jsonify_displayable_output(
-                status=trau.JudgeStatus.CE,
+                status=trau.JudgeStatus.TLE,
                 time_ms=exec_stats.time_ms,
                 memory_kib=exec_stats.memory_kib,
                 score=0,
                 continue_next=True,
             )
-        elif exec_stats.time_ms > time_limit_ms:
+        elif exec_stats.exit_code != 0:
             json = trau.jsonify_displayable_output(
-                status=trau.JudgeStatus.TLE,
+                status=trau.JudgeStatus.RE,
                 time_ms=exec_stats.time_ms,
                 memory_kib=exec_stats.memory_kib,
                 score=0,
@@ -90,7 +83,10 @@ else:
                     score=0,
                     continue_next=True,
                 )
-print(json)
-outcome_path = os.environ.get(trau.exec_job_outcome_path_envvar())
-with open(outcome_path, "w") as f:
-    f.write(json)
+    print(json)
+    outcome_path = os.environ.get(trau.exec_job_outcome_path_envvar())
+    with open(f"{outcome_path}/out.json", "w") as f:
+        f.write(json)
+
+if __name__ == "__main__":
+    asyncio.run(main())
