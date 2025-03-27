899d14ac4369e03888b726b1e0526e59257a0cba
ZOI-dayo <zoizoi.dayo@gmail.com>
2025-03-27 15:59:13 +0900

    impl docker run

--- app/exec_app/src/main.rs
+++ app/exec_app/src/main.rs
@@ -1,12 +1,21 @@
+use bollard::container::{Config, CreateContainerOptions};
+use bollard::Docker;
 use flate2::read::GzDecoder;
+use judge_core::constant::env_var_exec::SCRIPT_PATH;
 use judge_exec_grpc::generated::execute_service_server::{ExecuteService, ExecuteServiceServer};
 use judge_exec_grpc::generated::{ExecuteRequest, ExecuteResponse};
+use std::env;
 use tar::Archive;
 use tonic::async_trait;
 use tonic::{transport::Server, Request, Response, Status};
 
 #[derive(Default)]
 pub struct ExecApp {}
+
+impl ExecApp {
+    const DOCKER_IMAGE_NAME: &'static str = "exec-container-image";
+    const DOCKER_CONTAINER_NAME: &'static str = "exec-container";
+}
 
 #[async_trait]
 impl ExecuteService for ExecApp {
@@ -19,9 +28,33 @@ impl ExecuteService for ExecApp {
         request.dependency.iter().for_each(|dep| {
             let tar = GzDecoder::new(&dep.outcome[..]);
             let mut archive = Archive::new(tar);
-            archive.unpack("./outcome").unwrap();
-            // TODO: Run the container and pass dep.outcome to it
+            // todo: error handling
+            archive.unpack(format!("/outcome/{}", dep.envvar)).unwrap();
         });
+        // todo: error handling
+        let docker_api = Docker::connect_with_socket_defaults().unwrap();
+        // todo: error handling
+        let exec_container_entry_point = env::var(SCRIPT_PATH).unwrap();
+        let env_vars: Vec<String> = request
+            .dependency
+            .iter()
+            .map(|dep| format!("{}=\"/outcome/{}\"", dep.envvar, dep.envvar))
+            .collect();
+        let create_container_response = docker_api
+            .create_container(
+                Some(CreateContainerOptions {
+                    name: ExecApp::DOCKER_CONTAINER_NAME,
+                    platform: None,
+                }),
+                Config {
+                    image: Some(ExecApp::DOCKER_IMAGE_NAME),
+                    env: Some(env_vars.iter().map(|s| s.as_str()).collect()),
+                    cmd: Some(vec![exec_container_entry_point.as_str()]),
+                    ..Default::default()
+                },
+            )
+            .await
+            .unwrap(); // todo: error handling
         Ok(Response::new(ExecuteResponse {
             output: None,
             outcome: vec![],
