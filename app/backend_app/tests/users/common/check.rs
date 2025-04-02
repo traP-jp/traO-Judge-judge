a474be51b0977119bdd21e352c7a143aa348b743
PonponJuice <ponjuice2021@gmail.com>
2025-04-02 16:34:24 +0900

    feat: add postProblems and submitProblems fields to user response

--- app/backend_app/tests/users/common/check.rs
+++ app/backend_app/tests/users/common/check.rs
@@ -9,6 +9,27 @@ pub fn users_check_by_id(id: i64, resp_json: &mut Value) -> anyhow::Result<()> {
             "traqId": null,
             "githubId": "test_github_id_1",
             "iconUrl": null,
+            "postProblems": {
+                "problems": [
+                    {
+                        "id": 1,
+                        "title": "test",
+                        "authorId": 1,
+                        "isPublic": true,
+                        "difficulty": 1,
+                        "timeLimit": 10,
+                        "memoryLimit": 10,
+                        "solvedCount": 0,
+                        "createdAt": "2023-01-01T09:15:32Z",
+                        "updatedAt": "2023-01-01T10:20:47Z"
+                    },
+                ],
+                "total": 1,
+            },
+            "submitProblems": {
+                "submissions": [],
+                "total": 0,
+            },
             "githubLink": "https://github.com/test_user_1",
             "xLink": null,
             "selfIntroduction": "test_self_introduction_1",
@@ -23,6 +44,14 @@ pub fn users_check_by_id(id: i64, resp_json: &mut Value) -> anyhow::Result<()> {
             "traqId": "test_traq_id_2",
             "githubId": null,
             "iconUrl": null,
+            "postProblems": {
+                "problems": [],
+                "total": 0,
+            },
+            "submitProblems": {
+                "submissions": [],
+                "total": 0,
+            },
             "githubLink": null,
             "xLink": "https://x.com/test_user_2",
             "selfIntroduction": "",
@@ -37,6 +66,14 @@ pub fn users_check_by_id(id: i64, resp_json: &mut Value) -> anyhow::Result<()> {
             "traqId": null,
             "githubId": null,
             "iconUrl": "https://icon.com/test_user_3",
+            "postProblems": {
+                "problems": [],
+                "total": 0,
+            },
+            "submitProblems": {
+                "submissions": [],
+                "total": 0,
+            },
             "githubLink": null,
             "xLink": null,
             "selfIntroduction": "",
