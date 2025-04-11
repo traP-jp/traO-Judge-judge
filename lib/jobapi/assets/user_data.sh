cf9ecfa3eedb226c8271c8afa18b4eef9a3b11bf
ZOI-dayo <zoizoi.dayo@gmail.com>
2025-04-11 21:09:28 +0900

    install and run docker

--- lib/jobapi/assets/user_data.sh
+++ lib/jobapi/assets/user_data.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo yum install -y git gcc >> /log.txt 2>&1
+sudo yum install -y git gcc docker >> /log.txt 2>&1
+
+sudo systemctl start docker >> /log.txt 2>&1
 
 aws s3 cp s3://trao-infra-resources/exec-app/exec-app /root/exec-app >> /log.txt 2>&1
 chmod +x /root/exec-app >> /log.txt 2>&1
