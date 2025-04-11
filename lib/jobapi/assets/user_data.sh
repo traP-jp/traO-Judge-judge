b0618fc65eedeefe3dc6e1031f5ed9b219fd4c45
ZOI-dayo <zoizoi.dayo@gmail.com>
2025-04-11 20:13:23 +0900

    rename exec-http-server to exec-app

--- lib/jobapi/assets/user_data.sh
+++ lib/jobapi/assets/user_data.sh
@@ -2,6 +2,6 @@
 
 sudo yum install -y git gcc >> /log.txt 2>&1
 
-aws s3 cp s3://trao-infra-resources/exec-http-server/exec-http-server /root/exec-http-server >> /log.txt 2>&1
-chmod +x /root/exec-http-server >> /log.txt 2>&1
-/root/exec-http-server >> /log.txt 2>&1
+aws s3 cp s3://trao-infra-resources/exec-app/exec-app /root/exec-app >> /log.txt 2>&1
+chmod +x /root/exec-app >> /log.txt 2>&1
+/root/exec-app >> /log.txt 2>&1
