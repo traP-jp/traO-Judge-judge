f3c1a6d1ff03b5375e42fee3f3560859e0daa926
PonponJuice <ponjuice2021@gmail.com>
2025-04-30 16:48:25 +0900

    Add error handling for failed name retrieval in Builder

--- pylib/traopy_builder/src/builder.rs
+++ pylib/traopy_builder/src/builder.rs
@@ -250,6 +250,9 @@ impl Builder {
             let dep_to_name = dn_repo
                 .get_many(judge_resp.keys().cloned().collect())
                 .await
+                .map_err(|e| {
+                    PyErr::new::<pyo3::exceptions::PyValueError, _>(format!("Failed to get name: {:?}", e))
+                })?
                 .into_iter()
                 .map(|(id, name)| match name {
                     Some(name) => Ok((id, name)),
