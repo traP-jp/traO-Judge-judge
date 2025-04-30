02aeb21253cb7ae27b274ae9370c38314ba46598
PonponJuice <ponjuice2021@gmail.com>
2025-04-30 16:51:51 +0900

    change uuid to uuidRow

--- lib/backend_infra/src/model/icon.rs
+++ lib/backend_infra/src/model/icon.rs
@@ -1,9 +1,9 @@
 use domain::model::icon::Icon;
-use uuid::Uuid;
+use super::uuid::UuidRow;
 
 #[derive(Debug, Clone, sqlx::FromRow)]
 pub struct IconRow {
-    pub id: Uuid,
+    pub id: UuidRow,
     pub content_type: String,
     pub icon: Vec<u8>,
 }
@@ -11,7 +11,7 @@ pub struct IconRow {
 impl From<IconRow> for Icon {
     fn from(val: IconRow) -> Self {
         Icon {
-            id: val.id,
+            id: val.id.0,
             content_type: val.content_type,
             icon: val.icon,
         }
--- lib/backend_infra/src/repository/icon.rs
+++ lib/backend_infra/src/repository/icon.rs
@@ -3,7 +3,7 @@ use domain::{model::icon::Icon, repository::icon::IconRepository};
 use sqlx::MySqlPool;
 use uuid::Uuid;
 
-use crate::model::icon::IconRow;
+use crate::model::{icon::IconRow, uuid::UuidRow};
 
 #[derive(Clone)]
 pub struct IconRepositoryImpl {
@@ -20,7 +20,7 @@ impl IconRepositoryImpl {
 impl IconRepository for IconRepositoryImpl {
     async fn get_icon(&self, id: Uuid) -> anyhow::Result<Option<Icon>> {
         let icon = sqlx::query_as::<_, IconRow>("SELECT * FROM icons WHERE id = ?")
-            .bind(id)
+            .bind(UuidRow(id))
             .fetch_optional(&self.pool)
             .await?;
 
@@ -29,7 +29,7 @@ impl IconRepository for IconRepositoryImpl {
 
     async fn create_icon(&self, icon: Icon) -> anyhow::Result<()> {
         sqlx::query("INSERT INTO icons (id, content_type, icon) VALUES (?, ?, ?)")
-            .bind(icon.id)
+            .bind(UuidRow(icon.id))
             .bind(icon.content_type)
             .bind(&icon.icon)
             .execute(&self.pool)
