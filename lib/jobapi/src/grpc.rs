7baf1b8fa4cb905e1d4ebce81a363f43222fcc48
hayatroid <hayatroid@gmail.com>
2025-05-10 13:47:51 +0900

    fix: exec time for writer

--- lib/jobapi/src/grpc.rs
+++ lib/jobapi/src/grpc.rs
@@ -62,7 +62,7 @@ impl GrpcClient for GrpcClientType {
             .exec_client
             .execute(ExecuteRequest {
                 dependency: request,
-                exec_time_ms: 998244353,
+                exec_time_ms: 1800,
             })
             .await
             .unwrap()
