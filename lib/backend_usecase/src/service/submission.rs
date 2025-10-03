941655bd29d5e6bbf7b6c5dd536aa0905f675014
cho3881392697 <inoue.r.al@m.titech.ac.jp>
2025-10-03 15:44:21 +0900

    fix: overflow

--- lib/backend_usecase/src/service/submission.rs
+++ lib/backend_usecase/src/service/submission.rs
@@ -342,7 +342,7 @@ impl<
         );
         runtime_texts.insert(
             single_judge::MEMORY_LIMIT_KIB.to_string(),
-            (problem.memory_limit * 1024).to_string(),
+            (problem.memory_limit as i64 * 1024).to_string(),
         );
 
         let self_clone = std::sync::Arc::clone(self);
