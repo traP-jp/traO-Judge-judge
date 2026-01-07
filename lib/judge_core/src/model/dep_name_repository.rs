d32cec32cf176449e6298928b0426b62ef774552
comavius <comavius@proton.me>
2026-01-07 15:33:16 +0900

    dnrepo holds sha256 of resources

--- lib/judge_core/src/model/dep_name_repository.rs
+++ lib/judge_core/src/model/dep_name_repository.rs
@@ -7,15 +7,20 @@ pub trait DepNameRepository<IdType>: Clone + Send + Sync {
     async fn insert_many(
         &self,
         problem_id: IdType,
-        dep_id_to_name: HashMap<identifiers::DepId, String>,
+        dep_id_to_name: HashMap<identifiers::DepId, DepNameRecord>,
     ) -> Result<()>;
     async fn get_many(
         &self,
         dep_ids: Vec<identifiers::DepId>,
-    ) -> Result<HashMap<identifiers::DepId, Option<String>>>;
+    ) -> Result<HashMap<identifiers::DepId, Option<DepNameRecord>>>;
     async fn remove_many(&self, problem_id: IdType) -> Result<()>;
     async fn get_many_by_problem_id(
         &self,
         problem_id: IdType,
-    ) -> Result<HashMap<identifiers::DepId, String>>;
+    ) -> Result<HashMap<identifiers::DepId, DepNameRecord>>;
+}
+
+pub struct DepNameRecord {
+    pub name: String,
+    pub hash_sha256: String,
 }
