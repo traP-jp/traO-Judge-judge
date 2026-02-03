3392a8a882bcb2c8a595ab4f678bdec039bee4cd
comavius <comavius@proton.me>
2026-02-03 16:05:56 +0900

    add docs for job service

--- lib/backend_domain/src/service/job.rs
+++ lib/backend_domain/src/service/job.rs
@@ -4,17 +4,20 @@ use judge_core::model::job;
 /// `JobService` manages job queuing and processing lazily.
 #[async_trait]
 pub trait JobService {
+    /// Subscribe enqueued job by id if exists.
     async fn subscribe(
         &self,
         id: uuid::Uuid,
     ) -> Option<tokio::sync::oneshot::Receiver<JobResponse>>;
 
+    /// Eagerly start a job by id.
     async fn start(
         &self,
         id: uuid::Uuid,
         job: JobRequest,
     ) -> tokio::sync::oneshot::Receiver<JobResponse>;
 
+    /// Lazyly start a job by id.
     async fn subscribe_or_start(
         &self,
         id: uuid::Uuid,
