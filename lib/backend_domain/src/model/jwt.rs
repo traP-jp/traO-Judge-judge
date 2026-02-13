8578aa2dab3ec6821debb8b11acb8adbd4910a7b
comavius <comavius@proton.me>
2026-02-14 01:13:17 +0900

    refactor: newtyped id instantiation for jwt

--- lib/backend_domain/src/model/jwt.rs
+++ lib/backend_domain/src/model/jwt.rs
@@ -102,10 +102,7 @@ impl AuthToken {
 
         let auth_info = AuthInfo::decrypt(&token.claims.payload, encrypt_key)?;
 
-        Ok((
-            auth_info.email,
-            auth_info.user_id.map(|id| UserDisplayId(id)),
-        ))
+        Ok((auth_info.email, auth_info.user_id.map(|id| id.into())))
     }
 
     pub fn get_email(
@@ -157,7 +154,7 @@ impl AuthToken {
     }
 
     pub fn encode_email_update_jwt(
-        user_id: i64,
+        user_id: UserDisplayId,
         email: &str,
         encode_key: &str,
         encrypt_key: &str,
@@ -167,7 +164,7 @@ impl AuthToken {
         let nbf = Utc::now().timestamp();
 
         let auth_info = AuthInfo {
-            user_id: Some(user_id),
+            user_id: Some(user_id.into()),
             email: Some(email.to_string()),
             google_oauth: None,
             github_oauth: None,
