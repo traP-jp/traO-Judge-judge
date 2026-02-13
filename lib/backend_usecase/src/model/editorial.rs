56581ba987e1bf8d6739789bac5d0e0705b4e181
comavius <comavius@proton.me>
2026-02-14 01:40:05 +0900

    refactor: usecase models should use newtyped id types

--- lib/backend_usecase/src/model/editorial.rs
+++ lib/backend_usecase/src/model/editorial.rs
@@ -1,6 +1,9 @@
 use sqlx::types::chrono;
 
+use domain::model::editorial::EditorialId;
 use domain::model::editorial::{Editorial, EditorialSummary};
+use domain::model::problem::ProblemId;
+use domain::model::user::UserDisplayId;
 
 #[derive(Debug, Clone)]
 pub struct CreateEditorialData {
@@ -18,9 +21,9 @@ pub struct UpdateEditorialData {
 
 #[derive(Debug, Clone)]
 pub struct EditorialDto {
-    pub id: String,
-    pub problem_id: i64,
-    pub author_id: i64,
+    pub id: EditorialId,
+    pub problem_id: ProblemId,
+    pub author_id: UserDisplayId,
     pub title: String,
     pub statement: String,
     pub created_at: chrono::DateTime<chrono::Utc>,
@@ -31,7 +34,7 @@ pub struct EditorialDto {
 impl From<Editorial> for EditorialDto {
     fn from(value: Editorial) -> Self {
         EditorialDto {
-            id: value.id.to_string(),
+            id: value.id,
             problem_id: value.problem_id,
             author_id: value.author_id,
             title: value.title,
@@ -45,9 +48,9 @@ impl From<Editorial> for EditorialDto {
 
 #[derive(Debug, Clone)]
 pub struct EditorialSummaryDto {
-    pub id: String,
-    pub problem_id: i64,
-    pub author_id: i64,
+    pub id: EditorialId,
+    pub problem_id: ProblemId,
+    pub author_id: UserDisplayId,
     pub title: String,
     pub created_at: chrono::DateTime<chrono::Utc>,
     pub updated_at: chrono::DateTime<chrono::Utc>,
@@ -57,7 +60,7 @@ pub struct EditorialSummaryDto {
 impl From<EditorialSummary> for EditorialSummaryDto {
     fn from(value: EditorialSummary) -> Self {
         EditorialSummaryDto {
-            id: value.id.to_string(),
+            id: value.id,
             problem_id: value.problem_id,
             author_id: value.author_id,
             title: value.title,
